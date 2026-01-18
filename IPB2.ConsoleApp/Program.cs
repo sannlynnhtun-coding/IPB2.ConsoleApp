b26fbf9a50ec19751a0011c4837f6726d2a2a373
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2026-01-18 10:57:26 +0630

    Add project files.

---
+++ IPB2.ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("a");
+Console.WriteLine("b");
+Console.WriteLine("c");
+Console.WriteLine("d");
+Console.WriteLine("e");
+
+Console.ReadLine();
